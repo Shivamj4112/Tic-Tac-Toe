59b56d15109e6d3cc8ada3d6895d8f1aa444c86b
shiva <shivamjadav4112003@gmail.com>
2022-03-17 10:59:56 +0530

    commit

--- app/src/main/java/com/game/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/game/tic_tac_toe/MainActivity.java
@@ -3,8 +3,13 @@ package com.game.tic_tac_toe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView box101 , box102 , box103;
+    ImageView box201, box202, box203;
+    ImageView box301, box302, box303;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
